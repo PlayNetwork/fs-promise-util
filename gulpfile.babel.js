a36f7cf14eda196ec7460a4db957836443a2f372
Deepak Dahal <ddahal@playnetwork.com>
2017-04-10 10:34:07 -0700

    address cr comment: fix code coverage report

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
 import babel from 'gulp-babel';
+import coveralls from 'gulp-coveralls';
 import del from 'del';
 import eslint from 'gulp-eslint';
 import gulp from 'gulp';
+import gulpUtil from 'gulp-util';
+import istanbul from 'gulp-istanbul';
 import sourcemaps from 'gulp-sourcemaps';
 
 gulp.task('clean', () => {
@@ -17,6 +20,39 @@ gulp.task('build', ['clean'], () => {
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
 });
+
+gulp.task('clean-reports', () => {
+		return del('reports', { force : true });
+	});
+
+
+gulp.task('coveralls', ['test-coverage'], function () {
+	return gulp
+		.src('reports/lcov.info')
+		.pipe(coveralls());
+});
+
+gulp.task('test-coverage', ['build'], function () {
+	return gulp
+		.src(['./dist/**/*.js'])
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire())
+		.on('finish', function () {
+			gulp
+				.src(['./test/**/*.js'])
+				.pipe(mocha({ reporter : 'spec' })
+						.on('error', function (err) {
+							if (err.showStack) {
+								gulpUtil.log(err);
+							}
+
+							/* eslint no-invalid-this:0 */
+							this.emit('end');
+						}))
+				.pipe(istanbul.writeReports('./reports'));
+		});
+});
+
 
 gulp.task('lint', () => {
 	return gulp
